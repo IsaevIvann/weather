7d53467d7f8da5bfbd16b30b1423743a4d013f11
user <ivan.isaev7@icloud.com>
2025-09-19 19:09:42 +0300

    + new icons + timezone

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from telegram import Bot, Update, ReplyKeyboardMarkup
 from telegram.ext import Application, MessageHandler, ContextTypes, CommandHandler, filters
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from datetime import datetime, timedelta
-from x import timezone
+from datetime import timezone
 
 BOT_TOKEN = '7044099465:AAEKAmQZ5B-JFNLZgA5Ze661m6_FzQCpa4Y'
 USER_CHAT_IDS = ['457829882','191742166']
